076dd68370bea7d08db8136d10e1341f0c9bd362
rhymedys <rhymedys@gmail.com>
2017-04-02 20:14:13 +0800

    更新配置

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys
  * @Date:   2017-02-02 16:22:21
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2017-04-02 19:18:30
+ * @Last Modified time: 2017-04-02 20:07:32
  */
 
 'use strict'
@@ -11,18 +11,25 @@ import timeUtils from '../../utils/TimeUtils'
 import Vue from 'vue'
 
 // vuex
-import {mapState, mapGetters, mapActions, mapMutations} from 'vuex'
+import {
+  mapState,
+  mapGetters,
+  mapActions,
+  mapMutations
+} from 'vuex'
 import * as mActions from '../../vuex/Actions'
 import * as mGetters from '../../vuex/Getters'
 import * as mMutations from '../../vuex/Mutations'
-import {Field} from 'mint-ui'
+import {
+  Field
+} from 'mint-ui'
 Vue.component(Field.name, Field)
 export default {
-  currentPageName : 'login',
+  currentPageName: 'login',
   data() {
     return {}
   },
-  components : {},
-  computed : {},
-  methods : {}
+  components: {},
+  computed: {},
+  methods: {}
 }
